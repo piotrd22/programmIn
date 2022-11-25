ad6f6ca6e5a5154a1673edfbce33845ddf7a164c
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-25 11:57:22 +0100

    updated schema

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,16 +1,51 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 30,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    surname: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    backPicture: {
+      type: String,
+      default: "",
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  surname: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 30,
-  },
-});
+  { timestamps: true }
+);
+
+module.export = mongoose.model("User", UserSchema);
