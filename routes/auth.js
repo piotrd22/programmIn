16716c1fce31ce609d48a5f47c8fea498dd6b416
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-26 13:01:15 +0100

    basic signup system

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,33 @@ router.post("/signup", async (req, res) => {
     res.status(200);
     res.send(newUser);
   } catch (error) {
-    console.log(error);
+    res.status(500);
+    res.send(error);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const log_user = await User.findOne({
+      email: req.body.email,
+    });
+    if (res.status(404) && !log_user) {
+      res.send("404 USER NOT FOUND");
+    }
+
+    const userPassw = await bcrypt.compare(
+      req.body.password,
+      log_user.password
+    );
+    if (res.status(404) && !userPassw) {
+      res.send("404 WRONG PASSWORD");
+    }
+
+    res.status(200);
+    res.send(log_user);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
   }
 });
 
