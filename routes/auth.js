b19fd0a1ebad9f014104459ef7ea42a415a288dd
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-26 12:42:14 +0100

    added bcrypt to my password

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,23 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 const User = require("../schemas/UserSchema");
 
 router.post("/signup", async (req, res) => {
-  const newUser = await new User({
-    name: req.body.name,
-    surname: req.body.surname,
-    email: req.body.email,
-    password: req.body.password,
-    gender: req.body.gender,
-    nationality: req.body.nationality,
-  });
-
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassw = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: hashedPassw,
+      gender: req.body.gender,
+      nationality: req.body.nationality,
+    });
+
     await newUser.save();
+
     res.status(200);
     res.send(newUser);
   } catch (error) {
