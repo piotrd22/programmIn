0000789a8eae9a0915f1e430429bc455f6517b0e
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-11-29 21:14:29 +0100

    prettier all files -- added postControllers in api

---
+++ api/controllers/postsControllers.js
@@ -0,0 +1,13 @@
+const Post = require("../schemas/PostSchema");
+
+const createPost = async (req, res) => {
+  try {
+    const newPost = new Post(req.body);
+
+    await newPost.save();
+
+    res.status(200).send(newPost);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
