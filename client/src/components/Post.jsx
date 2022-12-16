1ba7fae7723d5f2ebcf2569f00d02039443ce6c1
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-12-16 17:09:50 +0100

    added Post scss and icons

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,16 +1,26 @@
+import { AiFillLike } from "react-icons/ai";
+import { FaComments } from "react-icons/fa";
+
 function Post({ post }) {
   return (
-    <div>
-      <div>
+    <div className="post">
+      <div className="post-div">
+        <span>Created by</span>
         <span className="date">{post.createdAt.slice(0, 10)}</span>
       </div>
-      <div>
+      <div className="post-desc">
         <span>{post?.desc}</span>
         {post.image && <p>Zdjecie</p>}
       </div>
-      <div>
-        <p>Likes {post.likes.length}</p>
-        <p>Comments {post.comments.length}</p>
+      <div className="post-likes">
+        <div>
+          <AiFillLike className="like-comment" />
+          {post.likes.length}
+        </div>
+        <div>
+          <FaComments className="like-comment" />
+          {post.comments.length}
+        </div>
       </div>
     </div>
   );
