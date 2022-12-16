b0db3011f12e46aa9fcadd7efc17e7fcd00242a4
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-12-16 16:22:23 +0100

    added sort of post by date

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -16,7 +16,7 @@ function Feed() {
       .then((res) => {
         setPosts(
           res.sort((p1, p2) => {
-            return new Date(p2.createdAt) - new Date(p1.createdAt);
+            return new Date(p2.updatedAt) - new Date(p1.updatedAt);
           })
         );
       })
