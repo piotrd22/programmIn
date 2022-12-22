6f33532d26942252114c5be5b242d7435f6b67de
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2022-12-22 16:08:58 +0100

    new post schema

--- api/schemas/PostSchema.js
+++ api/schemas/PostSchema.js
@@ -7,6 +7,10 @@ const PostSchema = new Schema(
       type: String,
       required: true,
     },
+    username: {
+      type: String,
+      required: true,
+    },
     image: {
       type: String,
     },
