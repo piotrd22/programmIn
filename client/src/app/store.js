e3f5a8a64cf1f61dd21baded19021f6e8e9c07e8
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-01-04 13:28:39 +0100

    added jwt verify middleware

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import jwt_decode from "jwt-decode";
 import authReducer from "../features/auth/authSlice";
 import postReducer from "../features/post/postSlice";
 import userReducer from "../features/user/userSlice";
+
+const refreshPage = () => window.location.reload();
+
+const checkTokenExpirationMiddleware = (store) => (next) => (action) => {
+  const token =
+    JSON.parse(localStorage.getItem("user")) &&
+    JSON.parse(localStorage.getItem("user"))["accessToken"];
+  if (!token) next(action);
+  else if (jwt_decode(token).exp < Date.now() / 1000) {
+    localStorage.clear();
+    refreshPage();
+    next(action);
+  }
+  next(action);
+};
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +28,5 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(checkTokenExpirationMiddleware),
 });
